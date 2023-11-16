8ac6bb1a2245839c2d7fdcec1f923e39d6125bdf
hemap <hemaprabha.rct@gmail.com>
2023-11-16 10:10:08 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
+import AddCourse from './components/AddCourse';
+import SearchCourse from './components/SearchCourse';
+import DeleteCourse from './components/DeleteCourse';
+import CourseViewall from './components/CourseViewall';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div >
+     <AddCourse/> 
+     <SearchCourse/>
+     <DeleteCourse/>
+     <CourseViewall/>
+       
     </div>
   );
 }
---
+++ src/components/AddCourse.jsx
@@ -0,0 +1,58 @@
+import React from 'react'
+import CourseHeader from './CourseHeader'
+
+const AddCourse = () => {
+  return (
+    <div>
+
+    <CourseHeader/>
+        <div className="container">
+            <div className="row">
+                <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                    <div className="row g-3">
+                        <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+
+                            <label htmlFor="" className="form-label">Course title</label>
+                            <input type="text" className="form-control" />
+
+                        </div>
+                        <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+
+                        <label htmlFor="" className="form-label">Duration</label>
+                        <input type="text" className="form-control" />
+
+                        </div>
+                        <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+
+                        <label htmlFor="" className="form-label">Description</label>
+                        <textarea name="" id="" cols="30" rows="10" className="form-control"></textarea>
+
+                        </div>
+                        <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+
+
+                        <label htmlFor="" className="form-label">Venue</label>
+                        <input type="text" className="form-control" />
+
+                        </div>
+                        <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+
+                        <label htmlFor="" className="form-label">Date</label>
+                        <input type="date" name="" id="" className="form-control" />
+
+                        </div>
+                        <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+
+                            <button className="btn btn-success">Submit</button>
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    </div>
+  )
+}
+
+export default AddCourse
